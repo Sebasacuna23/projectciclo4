af854a273220e27f9ddeba4c51841768bff60f2d
Sebastian <sebastian.acuna.mt@usa.edu.co>
2022-12-07 14:02:42 -0500

    actualizacion

---
+++ api-rest-papeleria/src/main/java/co/edu/unab/apirestpapeleria/controladores/AdministradorControlador.java
@@ -0,0 +1,40 @@
+package co.edu.unab.apirestpapeleria.controladores;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.unab.apirestpapeleria.Servicios.AdministradorServicio;
+import co.edu.unab.apirestpapeleria.modelos.AdministradorModelo;
+
+@RestController
+@RequestMapping("/admin")
+public class AdministradorControlador {
+    @Autowired
+    
+    AdministradorServicio administradorServicio;
+
+    @GetMapping()
+    public List<AdministradorModelo> getAllAdminOrden(){
+        return administradorServicio.getListAdminOrden();
+    }
+
+    @PostMapping()
+    public AdministradorModelo savePedido(@RequestBody AdministradorModelo admin){
+       return administradorServicio.guardarAdmin(admin);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteAdminById(@PathVariable("id") String id){
+       return administradorServicio.eliminarAdminPorId(id);
+        
+    }
+    
+}
---
+++ api-rest-papeleria/src/main/java/co/edu/unab/apirestpapeleria/modelos/AdministradorModelo.java
@@ -0,0 +1,25 @@
+package co.edu.unab.apirestpapeleria.modelos;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document("administrador")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AdministradorModelo {
+    @Id
+    private String id;
+    private String nombre;
+    private String apellido;
+    private Long ndocumento;
+    private Object datospersonales;
+
+    
+}
